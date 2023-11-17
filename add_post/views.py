305f3a298a74b333e4f39f7dd8a9c2d64eb96543
Glenngt <glenn.mattackal@gmail.com>
2023-11-17 22:33:33 +0530

    Created Serializer and Views

--- add_post/views.py
+++ add_post/views.py
@@ -2,12 +2,21 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from add_post.serializer import PostSerializer
+from add_post.models import AddPostModel
 
 # Create your views here.
 @csrf_exempt
 def addPost(request):
     if request.method == "POST":
-        return HttpResponse(json.dumps({"Status":"Post Added Successfully"}))
+        recieved_data = json.loads(request.body)
+        print(recieved_data)
+        serializer_check = PostSerializer(data=recieved_data)
+        if serializer_check.is_valid():
+            serializer_check.save()
+            return HttpResponse(json.dumps({"Status":"Post Added Successfully"}))
+        else:
+            return HttpResponse(json.dumps({"Status":"Post Adding Failed"}))
     
 @csrf_exempt
 def deletePost(request):
@@ -22,4 +31,7 @@ def searchPost(request):
 @csrf_exempt
 def viewPost(request):
     if request.method == "POST":
-        return HttpResponse(json.dumps({"Status":"Viewing Post"}))
+        postList = AddPostModel.objects.all()
+        seralized_data = PostSerializer(postList,many=True)
+        return HttpResponse(json.dumps(seralized_data.data))
+    
